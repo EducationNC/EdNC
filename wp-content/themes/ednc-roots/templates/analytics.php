92b4cba797d2f6afd7b6f6bcbfbe46528c2317c0
isabisa <alisa.r.herr@gmail.com>
2014-12-19 08:42:51 -0500

    Add demographics tracking to GA

--- wp-content/themes/ednc-roots/templates/analytics.php
+++ wp-content/themes/ednc-roots/templates/analytics.php
@@ -5,6 +5,7 @@
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-57754133-1', {'siteSpeedSampleRate': 50});
+ga('require', 'displayfeatures');
 ga('send', 'pageview');
 
 </script>
