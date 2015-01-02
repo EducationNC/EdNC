a8716adf7c70428f77adc302837ba16e5c03ce2d
isabisa <alisa.r.herr@gmail.com>
2015-01-02 12:16:55 -0500

    Add front end styles to TinyMCE editor

--- wp-content/themes/ednc-roots/lib/extras.php
+++ wp-content/themes/ednc-roots/lib/extras.php
@@ -26,3 +26,14 @@ function roots_wp_title($title) {
   return $title;
 }
 add_filter('wp_title', 'roots_wp_title', 10);
+
+// Load CSS to TinyMCE editor
+function add_mce_css( $mce_css ) {
+  if ( ! empty( $mce_css ) )
+  $mce_css .= ',';
+
+  $mce_css .= get_template_directory_uri() . '/assets/public/css/editor-style.css';
+
+  return $mce_css;
+}
+add_filter( 'mce_css', 'add_mce_css' );
--- wp-content/themes/ednc-roots/lib/init.php
+++ wp-content/themes/ednc-roots/lib/init.php
@@ -31,7 +31,7 @@ function roots_setup() {
   add_theme_support('html5', array('caption'));
 
   // Tell the TinyMCE editor to use a custom stylesheet
-  add_editor_style('/assets/css/editor-style.css');
+  // add_editor_style('/assets/css/editor-style.css');
 }
 add_action('after_setup_theme', 'roots_setup');
 
