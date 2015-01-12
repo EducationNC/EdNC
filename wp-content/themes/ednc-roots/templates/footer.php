a1170e71773a5e3bf789dd400a4da10e50a2fe56
isabisa <alisa.r.herr@gmail.com>
2015-01-12 17:43:43 -0500

    Remove BCBS logo from footer

--- wp-content/themes/ednc-roots/templates/footer.php
+++ wp-content/themes/ednc-roots/templates/footer.php
@@ -16,7 +16,6 @@ if ($time >= $launchtime) {
   <div class="above-footer">
     <ul class="list-inline text-center">
       <li><img src="<?php echo get_template_directory_uri(); ?>/assets/public/imgs/zsrf-logo-white-transparent.png" width="153" alt="Z. Smith Reynolds Foundation" />
-      <li><img src="<?php echo get_template_directory_uri(); ?>/assets/public/imgs/bcbsnclogo-gray-transparent.png" width="209" alt="Blue Cross Blue Shield North Carolina" />
       <li><img src="<?php echo get_template_directory_uri(); ?>/assets/public/imgs/sas_logo-white-transparent.png" width="78" alt="SAS Institute" />
       <li><img src="<?php echo get_template_directory_uri(); ?>/assets/public/imgs/o2-energies-inc-white-transparent.png" width="78" alt="O2energies" />
     </ul>
