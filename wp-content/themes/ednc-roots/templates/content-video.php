14993e3844717a8d4db78b15d6f31e1206faf82f
isabisa <alisa.r.herr@gmail.com>
2015-01-12 19:17:27 -0500

    Fix article header width on video posts

--- wp-content/themes/ednc-roots/templates/content-video.php
+++ wp-content/themes/ednc-roots/templates/content-video.php
@@ -1,7 +1,7 @@
 <article <?php post_class('article'); ?>>
     <header class="entry-header container">
       <div class="row">
-        <div class="col lg-7 col-md-9 col-centered">
+        <div class="col-lg-7 col-md-9 col-centered">
           <h1 class="entry-title"><?php the_title(); ?></h1>
           <?php get_template_part('templates/entry-meta'); ?>
           <?php get_template_part('templates/social', 'share'); ?>
