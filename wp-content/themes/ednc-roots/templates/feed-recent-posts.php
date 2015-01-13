adfd6c7917bafa28235dbf9dadc72fdb58c502be
isabisa <alisa.r.herr@gmail.com>
2015-01-13 15:58:54 -0500

    Modify layout of thumbnail in feed

--- wp-content/themes/ednc-roots/templates/feed-recent-posts.php
+++ wp-content/themes/ednc-roots/templates/feed-recent-posts.php
@@ -94,8 +94,8 @@ xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
       if ($image_sized) {
         $image_post = get_post($image_id);
         echo '<table border="0" cellspacing="0" cellpadding="0" width="600" style="font-family: Arial; sans-serif; color: #2b3e50;">';
-        echo '<tr style="width: 150px; max-width: 25%">';
-        echo '<td>';
+        echo '<tr>';
+        echo '<td style="width: 150px; max-width: 25%">';
         echo '<figure style="margin: 0 15px 0 0;">';
         if ($image_src) {
           echo '<img src="' . $image_sized['url'] . '" style="max-width: 100%;" />';
