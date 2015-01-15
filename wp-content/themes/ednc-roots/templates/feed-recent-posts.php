650b2cd17c89ec175fe68f01405f9abd08125392
isabisa <alisa.r.herr@gmail.com>
2015-01-15 15:13:18 -0500

    Remove image captions

--- wp-content/themes/ednc-roots/templates/feed-recent-posts.php
+++ wp-content/themes/ednc-roots/templates/feed-recent-posts.php
@@ -96,14 +96,9 @@ xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
         echo '<table id="templateRows" border="0" cellspacing="0" cellpadding="0" width="600" style="font-family: Arial; sans-serif; color: #2b3e50;">';
         echo '<tr>';
         echo '<td style="width: 150px; max-width: 25%" class="templateColumnContainer">';
-        echo '<figure style="margin: 0 15px 0 0;">';
         if ($image_src) {
-          echo '<img src="' . $image_sized['url'] . '" />';
+          echo '<img src="' . $image_sized['url'] . '" style="max-width: 100%;" />';
         }
-        echo '<figcaption style="font-style: italic;">';
-        echo $image_post->post_excerpt;
-        echo '</figcaption>';
-        echo '</figure>';
         echo '</td>';
         echo '<td class="templateColumnContainer">';
         echo '<div style="padding-left: 15px;">';
