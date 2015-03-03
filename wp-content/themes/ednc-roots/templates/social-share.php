ff85eb9841b57f292c2e9d2dd35b8e7deb0e70b1
isabisa <alisa.r.herr@gmail.com>
2015-03-03 15:23:34 -0500

    Switch social share buttons to use ShareThis

--- wp-content/themes/ednc-roots/templates/social-share.php
+++ wp-content/themes/ednc-roots/templates/social-share.php
@@ -1,21 +1,7 @@
-<?php
-// get post thumbnail url for buffer
-if (has_post_thumbnail()) {
-  $thumb_id = get_post_thumbnail_id();
-  $thumb_url = wp_get_attachment_image_src($thumb_id, 'full', true);
-}
-?>
-<ul class="social-buttons list-inline">
-  <li>
-    <div class="fb-share-button" data-href="<?php echo get_permalink(); ?>" data-layout="button_count"></div>
-  </li>
-  <li>
-    <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo get_permalink(); ?>" data-text="<?php the_title(); ?>" data-via="EducationNC"></a>
-  </li>
-  <li>
-    <div class="g-plus" data-action="share" data-annotation="bubble" data-href="<?php echo get_permalink(); ?>"></div>
-  </li>
-  <li>
-    <script type="IN/Share" data-url="<?php echo get_permalink(); ?>" data-counter="right" data-showzero="true" data-onsuccess="track_linkedin_share"></script>
-  </li>
-</ul>
+<div class="social-buttons">
+  <span class='st_facebook_hcount' displayText='Facebook'></span>
+  <span class='st_twitter_hcount' displayText='Tweet'></span>
+  <span class='st_sharethis_hcount' displayText='ShareThis'></span>
+  <span class='st_email_hcount' displayText='Email'></span>
+  <span class='st_print_hcount' displayText='Print'></span>
+</div>
