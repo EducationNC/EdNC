2b4483f5a5d25ce281bfbe89658ee1782f7c22a8
isabisa <alisa.r.herr@gmail.com>
2015-03-31 14:45:54 -0400

    Disable share button activation on hover

--- wp-content/themes/ednc-roots/templates/social-scripts-header.php
+++ wp-content/themes/ednc-roots/templates/social-scripts-header.php
@@ -1,3 +1,3 @@
 <script type="text/javascript">var switchTo5x=true;</script>
 <script type="text/javascript" src="https://ws.sharethis.com/button/buttons.js"></script>
-<script type="text/javascript">stLight.options({publisher: "9370f123-7244-4151-a639-30ba1d71bf7f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+<script type="text/javascript">stLight.options({publisher: "9370f123-7244-4151-a639-30ba1d71bf7f", doNotHash: false, doNotCopy: false, hashAddressBar: false, onhover: false});</script>
