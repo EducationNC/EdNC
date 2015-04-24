d4ae5fb5e874ec72f047dba1962ad357451424a6
isabisa <alisa.r.herr@gmail.com>
2015-04-24 09:01:23 -0400

    Template changes for upcoming crossover deadline

--- wp-content/themes/ednc-roots/page-legislation-tracker.php
+++ wp-content/themes/ednc-roots/page-legislation-tracker.php
@@ -5,6 +5,7 @@
 
   <div class="row">
     <div class="col-md-3 col-md-push-9">
+
       <div class="callout">
         <h4>Search education bills</h4>
         <form method="get" action="<?php the_permalink() ?>">
@@ -31,55 +32,127 @@
     </div>
 
     <div class="col-md-9 col-lg-8 col-md-pull-3">
-      <?php
-      // $paged = get_query_var('paged') ? get_query_var('paged') : 1;
-      $args = array(
-        'post_type' => 'bill',
-        'posts_per_page' => -1,
-        'orderby' => 'menu_order',
-        'order' => 'ASC'
-        // 'paged' => $paged
-      );
-
-      if (isset($_GET['k'])) {
-        // $args['s'] = $_GET['k'];
-        $args['meta_query'] = array(
-          array(
-            'key' => 'short_title',
-            'value' => $_GET['k'],
-            'compare' => 'LIKE'
-          )
-        );
-        echo '<h2>Search results for: <em>' . sanitize_text_field($_GET['k']) . '</em></h2>';
-      } else {
-        the_content();
-      }
-
-      $resources = new WP_Query( $args );
-
-      if ($resources->have_posts()) : while ( $resources->have_posts() ) : $resources->the_post(); ?>
-
-      <div class="clearfix">
-        <h3 class="resource-title"><a href="<?php the_field('link_to_bill'); ?>" target="_blank"><?php the_title() ?>: <?php echo the_field('short_title'); ?></a></h4>
-        <?php if (get_field('description')) { ?>
-        <p><?php the_field('description'); ?></p>
-        <?php } ?>
-      </div>
 
       <?php
-      endwhile;
+        // If this is a search result
+        if (isset($_GET['k'])) {
+          $args = array(
+            'post_type' => 'bill',
+            'posts_per_page' => -1,
+            'orderby' => 'menu_order',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'short_title',
+                'value' => $_GET['k'],
+                'compare' => 'LIKE'
+              )
+            )
+          );
+          echo '<h2>Search results for: <em>' . sanitize_text_field($_GET['k']) . '</em></h2>';
 
-      if ($resources->max_num_pages > 1) { ?>
-        <nav class="post-nav">
-          <?php wp_pagenavi( array( 'query' => $resources ) ); ?>
-        </nav>
-      <?php } ?>
+          $bills = new WP_Query( $args );
 
-      <?php else : ?>
+          if ($bills->have_posts()) : while ( $bills->have_posts() ) : $bills->the_post();
+            get_template_part('templates/content', 'bill');
+          endwhile; else :
+            echo '<h3>No resources found</h3>';
+          endif; wp_reset_query();
 
-        <h3>No resources found</h3>
+        } else {
+          // If not a search result, lay out in sections
+          the_content();
 
-      <?php endif; wp_reset_query(); ?>
+          $args = array(
+            'post_type' => 'bill',
+            'posts_per_page' => -1,
+            'orderby' => 'menu_order',
+            'order' => 'ASC',
+            'tax_query' => array(
+              array(
+                'taxonomy' => 'bill-status',
+                'field' => 'slug',
+                'terms' => 'ratified'
+              )
+            )
+          );
+
+          $bills = new WP_Query( $args );
+
+          if ($bills->have_posts()) :
+            echo '<h2>2015-16 ratified bills</h2>';
+            while ( $bills->have_posts() ) : $bills->the_post();
+              get_template_part('templates/content', 'bill');
+            endwhile;
+          endif; wp_reset_query();
+
+          echo '<hr />';
+
+          $args = array(
+            'post_type' => 'bill',
+            'posts_per_page' => -1,
+            'orderby' => 'menu_order',
+            'order' => 'ASC',
+            'tax_query' => array(
+              'relation' => 'AND',
+              array(
+                'taxonomy' => 'bill-status',
+                'field' => 'slug',
+                'terms' => 'ratified',
+                'operator' => 'NOT IN'
+              ),
+              array(
+                'taxonomy' => 'bill-status',
+                'field' => 'slug',
+                'terms' => 'met-crossover-deadline'
+              )
+            )
+          );
+
+          $bills = new WP_Query( $args );
+
+          if ($bills->have_posts()) :
+            echo '<h2>2015-16 bills in play</h2>';
+            while ( $bills->have_posts() ) : $bills->the_post();
+              get_template_part('templates/content', 'bill');
+            endwhile;
+          endif; wp_reset_query();
+
+          echo '<hr />';
+
+          $args = array(
+            'post_type' => 'bill',
+            'posts_per_page' => -1,
+            'orderby' => 'menu_order',
+            'order' => 'ASC',
+            'tax_query' => array(
+              'relation' => 'AND',
+              array(
+                'taxonomy' => 'bill-status',
+                'field' => 'slug',
+                'terms' => 'ratified',
+                'operator' => 'NOT IN'
+              ),
+              array(
+                'taxonomy' => 'bill-status',
+                'field' => 'slug',
+                'terms' => 'met-crossover-deadline',
+                'operator' => 'NOT IN'
+              )
+            )
+          );
+
+          $bills = new WP_Query( $args );
+
+          if ($bills->have_posts()) :
+            echo '<h2>2015-16 bills did not meet crossover deadline</h2>';
+            while ( $bills->have_posts() ) : $bills->the_post();
+              get_template_part('templates/content', 'bill');
+            endwhile;
+          endif; wp_reset_query();
+
+        }
+      ?>
     </div>
   </div>
 <?php endwhile; ?>
