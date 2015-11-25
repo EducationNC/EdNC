aa2a84c0db056d0ae968d48c01cac5a080dd4356
isabisa <alisa.r.herr@gmail.com>
2015-11-25 15:36:48 -0500

    Add Google AdSense to top right of home page

---
+++ wp-content/themes/ednc-roots/templates/sidebar-home-adsense.php
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Home sidebar -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2642458473228537"
+     data-ad-slot="3971198601"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
