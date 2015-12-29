5312e8969524e6b4416ec6fff5a9f97112a28c25
isabisa <alisa.r.herr@gmail.com>
2015-12-29 18:01:26 -0500

    Work on about page template

--- wp-content/themes/ednc-2016/page-about.php
+++ wp-content/themes/ednc-2016/page-about.php
@@ -4,9 +4,11 @@
 
     <?php get_template_part('templates/layouts/content', 'page-right-aside'); ?>
 
-    <div class="sep"></div>
-
-    <h2>EdNC <strong>Staff</strong></h2>
+    <div class="row">
+      <div class="col-xs-12">
+        <h3 class="section-header">EdNC Staff</h3>
+      </div>
+    </div>
 
     <div class="row">
       <?php
@@ -22,7 +24,7 @@
 
       if ($about->have_posts()) : while ($about->have_posts()) : $about->the_post(); ?>
 
-      <div class="col-md-6 has-photo-overlay">
+      <div class="col-md-6">
         <div class="row">
           <div class="col-xs-5">
             <a href="<?php the_permalink(); ?>">
@@ -44,25 +46,4 @@
       <?php endwhile; endif; wp_reset_query(); ?>
     </div>
   </div>
-
-  <div class="container-fluid timeline-wrap">
-    <script type="text/javascript" src="https://s3.amazonaws.com/cdn.knightlab.com/libs/timeline/latest/js/storyjs-embed.js"></script>
-    <script type="text/javascript">
-    jQuery(document).ready(function($) {
-      createStoryJS({
-        type:           'timeline',
-        width:          '100%',
-        height:         '450',
-        source:         'https://docs.google.com/spreadsheets/d/1bQ6PZPO_bRtRYmRygt8MqGxhZzsu-S2nHHGEjbzIwoI/pubhtml',
-        embed_id:       'timeline-100',
-        start_at_slide: 1
-      });
-    });
-    </script>
-
-    <div class="timeline-100 hidden-sm hidden-xs">
-      <div id="timeline-100"></div>
-    </div>
-  </div>
-
 <?php endwhile; ?>
